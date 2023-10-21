64ff834b034fb27269b26a9ea5e6852122ea9f6b
Skowsome <119661128+Skowsome@users.noreply.github.com>
2023-10-21 17:09:15 +0500

    asafdsd

---
+++ Finance Management/app.js
@@ -0,0 +1,30 @@
+var currentTab = 0;
+
+
+
+function changeTab(index){
+    if (index===0) {
+        document.getElementById('0').style.display = 'block';
+        document.getElementById('1').style.display = 'none';
+        document.getElementById('2').style.display = 'none';
+        document.getElementById('3').style.display = 'none';
+    } else if(index===1){
+        document.getElementById('0').style.display = 'none';
+        document.getElementById('1').style.display = 'block';
+        document.getElementById('2').style.display = 'none';
+        document.getElementById('3').style.display = 'none';
+    } 
+    else if(index===2){
+        document.getElementById('0').style.display = 'none';
+        document.getElementById('1').style.display = 'none';
+        document.getElementById('2').style.display = 'block';
+        document.getElementById('3').style.display = 'none';
+    }
+    else if(index===3){
+        document.getElementById('0').style.display = 'none';
+        document.getElementById('1').style.display = 'none';
+        document.getElementById('2').style.display = 'none';
+        document.getElementById('3').style.display = 'block';
+    } 
+}
+
